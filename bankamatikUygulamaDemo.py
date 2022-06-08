443dd886434344b4c2fe4ac6044dfe1d9282f476
mhmtdrndr <83140766+mhmtdrndr@users.noreply.github.com>
2022-06-08 11:18:50 +0300

    Add files via upload
    menü güncellemesi yapıldı

--- bankamatikUygulamaDemo.py
+++ bankamatikUygulamaDemo.py
@@ -1,4 +1,29 @@
-amount = 1000
+'''
+    V.1.1
+'''
+
+def bakiyeDurum(amount):
+    print(f"Bakiyeniz : {amount}")
+    
+def paraYatir(amount):
+    yatirilanTutar = int(input("Yarılan parayı giriniz : "))
+    if yatirilanTutar < 0:
+        print("Girilen tutar hatalı")
+    else: 
+        amount = amount + yatirilanTutar
+        print(f"Para yatırıldı. Güncel bakiye : {amount}")
+        
+    return amount
+
+def paraCek(amount):
+    celilenTutar = int(input("Çekilen parayı giriniz : "))    
+    if celilenTutar > amount:
+        print("Bakiye yerli değil")
+    else:
+        amount = amount - celilenTutar
+        print(f"Para çekildi. Güncel bakiye : {amount}")
+    
+    return amount    
 
 print("------------------------------------")
 print(" --------- XXXXXXXXX BANK --------- ")
@@ -6,26 +31,23 @@ print("------------------------------------")
 
 print("Hoşgeldiniz ->")
 
-sec = ""
-while(sec != 0 ):
+amount = 1000
+durum = True
+while durum:
 
-    print("[1] [ Bakiye Sorma ]")
-    print("[2] [ Hesaba Para Yatırma ]")
-    print("[3] [ Hesaptan Para Çekme ]")
-    print("[0] [ ÇIKIŞ ]")
+    print("[1] [ Bakiye Sorma ........ ]")
+    print("[2] [ Hesaba Para Yatırma . ]")
+    print("[3] [ Hesaptan Para Çekme . ]")
+    print("[0] [ ÇIKIŞ ............... ]")
 
     sec = int(input("Lütfen seçiminizi yapınız : "))
   
     if (sec == 1):
-        print(f"Bakiyeniz : {amount}")
-    elif (sec == 2):
-        yatirilanTutar = int(input("Yarılan parayı giriniz : "))
-        amount = amount + yatirilanTutar
-        print(f"Para yatırıldı. Güncel bakiye : {amount}")
+        bakiyeDurum(amount)
+    elif (sec == 2):        
+        paraYatir(amount)
     elif (sec == 3):
-        celilenTutar = int(input("Çekilen parayı giriniz : "))
-        amount = amount - celilenTutar
-        print(f"Para çekildi. Güncel bakiye : {amount}")
+        paraCek(amount)
     else:
         print("Hoşçakalın")
-        break
+        durum = False
