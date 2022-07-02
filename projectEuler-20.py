626559dc916f005c7c676dd83d182c8926d5365e
Mehmet Derindere <83140766+mhmtdrndr@users.noreply.github.com>
2022-07-03 01:26:41 +0300

    Add files via upload

---
+++ projectEuler-20.py
@@ -0,0 +1,23 @@
+def factoriyelAl(number):
+    faktoriyel = 1
+    i = 1
+    while i <= number:
+        faktoriyel = faktoriyel * i
+        i += 1
+    return faktoriyel
+
+def rakamlariTopla(number):
+    toplam = 0
+    temp_number = number
+    while True:
+        basamak = temp_number % 10
+        toplam += basamak
+        temp_number = temp_number // 10
+        if temp_number == 0:
+            break
+    return toplam
+
+factoriyelNumber = int(input("Faktroyeli hesaplanacak olan sayı : "))
+
+basamakToplam = factoriyelAl(factoriyelNumber)
+print(rakamlariTopla(basamakToplam))
