20e186bd5a2df33b434ffb9c9572b0b0df91f500
Mehmet Derindere <83140766+mhmtdrndr@users.noreply.github.com>
2022-07-29 10:40:41 +0300

    Add files via upload

---
+++ projectEuler-7.py
@@ -0,0 +1,16 @@
+def isPrime(number):
+    for i in range(2,number):
+        if number % i == 0:
+            return False
+    return True
+
+primeNumberList = [2]
+value = 3
+
+while len(primeNumberList) < 10001:
+    if isPrime(value):
+        primeNumberList.append(value)
+    value += 1
+    
+print(primeNumberList)
+print(primeNumberList[-1])
